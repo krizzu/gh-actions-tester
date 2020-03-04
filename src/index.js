25b0c4ec8c72c5077e21340a0e7d33cabe2d4e42
Krzysztof Borowy <krizzu.dev@gmail.com>
2020-03-04 18:40:16 +0100

    test: eslint-check1.0.1

--- src/index.js
+++ src/index.js
@@ -3,11 +3,69 @@ function testMe() {
 
   for (let i = 0; i < a; i++) {
     console.log(a - i);
+    console.log(a - i);
+    console.log(a - i);
+    console.log(a - i);
+    console.log(a - i);
+    console.log(a - i);
+    console.log(a - i);
+    console.log(a - i);
 
 
-
-
+    let a = 123;
+    let b = 123;
   }
 }
+
+function nothing(){
+
+
+}
+
+
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
+nothing
 
 export default testMe;
